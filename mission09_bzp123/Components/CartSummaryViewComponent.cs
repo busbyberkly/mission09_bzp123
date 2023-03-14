caa8de6a64fe7a6aa9c418a62615f772304f9ec1
busbyberkly <bzpeterson0901@gmail.com>
2023-03-13 19:00:42 -0600

    Adding the cart graphic moment

---
+++ mission09_bzp123/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mission09_bzp123.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission09_bzp123.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
